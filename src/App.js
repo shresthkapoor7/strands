a88535cd0db067ec0355d871ba2986f1329fc17a
Shresth Kapoor <shresthkapoor7@gmail.com>
2025-05-25 04:15:26 -0400

    Routing fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
 import './App.css';
